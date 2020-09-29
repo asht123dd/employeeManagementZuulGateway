4620228c3f2e71cb7f250906e3d7bb3149dcfa38
Ashutosh Singh <ashutoshsinghwork@gmail.com>
2020-09-30 00:44:19 +0530

    adding code

---
+++ src/test/java/com/tcs/employeeManagementZuulServerWithOAuth/EmployeeManagementZuulServerWithOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.employeeManagementZuulServerWithOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementZuulServerWithOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
